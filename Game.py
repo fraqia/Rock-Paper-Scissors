db9e5408e89e2e249efc18c1b776a8def14879fe
fraqia <156285153+fraqia@users.noreply.github.com>
2024-03-25 18:33:39 +0200

    fix outer loop and added while True. Fixed player_hand function.

--- Game.py
+++ Game.py
@@ -2,37 +2,40 @@ from random import randint
 from fuzzywuzzy import process
 
 def player_hand(hand: str, player_hand_list: list) -> list:
-    options = ["1. Rock", "2. Paper", "3. Scissors"]
-    try:
-        if hand in ['1', '2', '3']:
-            closest_match = options[int(hand) - 1]
-            score = 100
-            # player_hand_list.append(closest_match)
-            # player_hand_list.append(player_hand)
-            # return player_hand_list
-        else:
-            closest_match, score = process.extractOne(hand, options)
-        # print(f"Closest match: {closest_match} (Score: {score})")
-        if score < 95:
-            print("Invalid choice! Please try again.")
-            return None  
-        if closest_match == "1. Rock":
-            player_hand = 1
-            closest_match = "Rock"
-        elif closest_match == "2. Paper":
-            player_hand = 2
-            closest_match = "Paper"
-        elif closest_match == "3. Scissors":
-            player_hand = 3
-            closest_match = "Scissors"
-        player_hand_list.append(closest_match)
-        player_hand_list.append(player_hand)
-        # print(player_hand_list)
-        print(f'You: {closest_match}')
-        return player_hand_list
-    except ValueError:
+    options = ["1. Rock", "2. Paper", "3. Scissors", "Rock", "Paper", "Scissors"]
+    # try:
+    player_hand = 0
+    if hand in ['1', '2', '3']:
+        closest_match = options[int(hand) - 1]
+        score = 100
+        # player_hand_list.append(closest_match)
+        # player_hand_list.append(player_hand)
+        # return player_hand_list
+    else:
+        closest_match, score = process.extractOne(hand, options)
+    print(f"Closest match: {closest_match} (Score: {score})")
+    if score < 95:
         print("Invalid choice! Please try again.")
         return None  
+    print((closest_match))
+    if closest_match == "1. Rock" or closest_match.lower() == "rock":
+        player_hand = 1
+        closest_match = "Rock"
+    elif closest_match == "2. Paper" or closest_match.lower() == "paper":
+        player_hand = 2
+        closest_match = "Paper"
+    elif closest_match == "3. Scissors" or closest_match.lower() == "scissors":
+        player_hand = 3
+        closest_match = "Scissors"
+    player_hand_list.append(closest_match)
+    player_hand_list.append(player_hand)
+    # print(player_hand_list)
+    print(f'You: {closest_match}')
+    print(player_hand_list)
+    return player_hand_list
+    # except ValueError:
+    #     print("Invalid choice! Please try again.")
+    #     return None  
 
 def computer_hand(c_hand_list) -> list:
     options = {"Rock":1, "Paper":2, "Scissors":3}
@@ -106,27 +109,32 @@ def game(player_hand_list: list, c_hand_list:list, results:dict):
     return results
 
 if __name__=="__main__":
-    try:
-        results = {"Wins": 0, "Loses": 0, "Draws": 0}
-        player_hand_list = []
-        c_hand_list = []
-        rounds = int(input('How many rounds would you like to play?\n'))
-        rounds_start = 1
-        while rounds_start <= rounds:
-            print("*"*50)
-            print(f"Round {rounds_start}:")
-            hand = input('Enter your choice (1.Rock/2.Paper/3.Scissors): ')
-            player_hand_list = player_hand(hand, player_hand_list)
-            c_hand_list = computer_hand(c_hand_list)
-            results = game(player_hand_list, c_hand_list, results)
-            rounds_start += 1
+    while True:
+        try:
+            results = {"Wins": 0, "Loses": 0, "Draws": 0}
             player_hand_list = []
             c_hand_list = []
-    except ValueError:
-        print("Invalid input! Please enter a valid number of rounds.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-    results_game(results, player_hand_list, c_hand_list)
+            rounds = int(input('How many rounds would you like to play?\n'))
+            rounds_start = 1
+            while rounds_start <= rounds:
+                print("*"*50)
+                print(f"Round {rounds_start}:")
+                hand = input('Enter your choice (1.Rock/2.Paper/3.Scissors): ')
+                player_hand_list = player_hand(hand, player_hand_list)
+                if player_hand_list == None:
+                    continue
+                c_hand_list = computer_hand(c_hand_list)
+                results = game(player_hand_list, c_hand_list, results)
+                rounds_start += 1
+                player_hand_list = []
+                c_hand_list = []
+            results_game(results, player_hand_list, c_hand_list)
+            break
+        except ValueError:
+            print("Invalid input! Please enter a valid number of rounds.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+        # results_game(results, player_hand_list, c_hand_list)
 
 
 
